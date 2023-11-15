d05c2e92c2b09c303c9d002c3b13bae8315b4ffa
SerenaHa12 <serenavander.ha@gmail.com>
2023-11-16 00:47:41 +0700

    setup tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+// import components
+import Header from "./components/Header";
+import Center from "./components/Center";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <Center />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      className="
+            header-container 
+            p-4 
+            fixed 
+            left-0 
+            bg-white 
+            data:bg-[#2b2c37] 
+            z-50 
+            right-0"
+    >
+      <header
+        className="
+            flex 
+            justify-between
+            dark:text-white 
+            items-center"
+      >
+        hi
+      </header>
+    </div>
+  );
+};
+
+export default Header;
